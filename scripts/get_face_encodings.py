6c79f6e348079627eb6cdce7235a56b8f649e393
Parkhi <parkhigoyal46@gmail.com>
2024-02-23 16:49:11 +0530

    saved_encodings

---
+++ scripts/get_face_encodings.py
@@ -0,0 +1,27 @@
+import os
+import face_recognition
+import pickle
+
+def get_face_encodings():
+    base_path = "D:\Work\AMS Everything\AMS\AMS/labeled_images/"
+    roll_no_to_face_encoding = {}
+    
+
+    for student_folder in os.listdir(base_path):
+        student_roll_no = student_folder
+        student_folder = base_path + student_folder
+        for image in os.listdir(student_folder):
+            image = student_folder + "/" + image
+            print(image)
+            image = face_recognition.load_image_file(image)
+            if(len(face_recognition.face_encodings(image)) == 0):
+                print("No face found in the image")
+                continue
+            roll_no_to_face_encoding[student_roll_no] = face_recognition.face_encodings(image)[0]
+
+    print(roll_no_to_face_encoding)
+    with open('face_encodings.dat', 'wb') as f:
+        pickle.dump(roll_no_to_face_encoding, f)
+
+
+   
---
+++ scripts/save_face_encodings.py
@@ -0,0 +1,3 @@
+from get_face_encodings import get_face_encodings
+
+get_face_encodings()
