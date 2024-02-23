a119d4217b5ebb0760eb1410835b97bd4871a8c6
Parkhi <parkhigoyal46@gmail.com>
2024-02-23 15:23:19 +0530

    frame extracted

---
+++ scripts/test.py
@@ -0,0 +1,11 @@
+from extract_frames import get_frames_from_video
+import os
+
+def main():
+    file_path = 'D:\Work\AMS Everything\AMS\AMS\scripts\pesting.mp4'
+    # os.chdir('directory_path')
+    
+    get_frames_from_video(file_path)
+
+if __name__ == "__main__":
+    main()
